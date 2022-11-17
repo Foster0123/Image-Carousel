549293df2f24c4e78be88a998db9b28e1d504826
Foster <72973015+Foster0123@users.noreply.github.com>
2022-11-17 21:51:03 +0530

    1st Carousel Commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const container = document.getElementById('container');
+const prev      = document.getElementById('prev');
+const next      = document.getElementById('next');
+var img       = document.getElementById('image');
+let count       = 1;
