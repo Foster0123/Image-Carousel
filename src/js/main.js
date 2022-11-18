2d9eb1ec7c334a2b178e32ccce413435a4a5bc39
Foster <72973015+Foster0123@users.noreply.github.com>
2022-11-19 00:23:30 +0530

    4th Carousel Commmit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
-const container = document.getElementById('container');
-const prev      = document.getElementById('prev');
-const next      = document.getElementById('next');
-var img       = document.getElementById('image');
-let count       = 1;
+const img = document.getElementById("image");
+const prev 	= document.getElementById('prev');
+const next  = document.getElementById('next');
+let count   = 1;
+
+next.addEventListener("click", (e) => {
+	count++;
+	img.src = `./img/bg-${count}.jpg`;
+	if(count == 4){
+		count = 0;
+	}
+	
+	console.log(count);
+
+})
+prev.addEventListener("click", (e) => {
+	img.src = `./img/bg-${count}.jpg`;
+	if(count == 0){
+		count = 5;
+	}
+	count--;
+	console.log(count);
+})
+console.log(count);
