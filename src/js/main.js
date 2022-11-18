2fd36a48f0afd04d9f6cc4c729e27e87687475b2
Foster <72973015+Foster0123@users.noreply.github.com>
2022-11-18 22:43:31 +0530

    3nd Carousel Commmit

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,3 @@ const prev      = document.getElementById('prev');
 const next      = document.getElementById('next');
 var img       = document.getElementById('image');
 let count       = 1;
-
-let e = document.getElementsByClassName("img");
-
-for(let i=0; i < e.length; i++) {
-	let h = e[i].clientHeight;
-	let w = e[i].clientWidth;
-	console.log(h, w);
-}
